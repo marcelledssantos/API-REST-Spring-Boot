17c00e392cd4555c9490353770e70d30aeb75625
Marcelle Santos <marcelledossantos2@gmail.com>
2023-12-03 17:14:03 -0300

    config o ambiente de testes

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,5 @@
+insert into  USUARIO (id, username, password,role) values (100, "marcelle@gmail.com","123456", "ROLE_ADMIN");
+
+insert into  USUARIO (id, username, password,role) values (101, "magali@gmail.com","123457", "ROLE_CLIENTE");
+
+insert into  USUARIO (id, username, password,role) values (102, "lucas@gmail.com","123458", "ROLE_CLIENTE");
